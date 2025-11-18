006d4e0b4a22e8bff205b31caf716710de17b958
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2025-11-18 09:49:11 +0100

    feat: Update story route to include dynamic page rendering and add story data

--- routes/story.js
+++ routes/story.js
@@ -1,10 +1,25 @@
 import express from "express"
+import fs from "fs"
 const router = express.Router()
+const storyData = JSON.parse(fs.readFileSync("./story/story.json", "utf-8"))
+const findpage = (id) => {
+    return storyData.find(page => page.id === id)
+}
 
 router.get("/", (req, res) => {
     res.render("story.njk", {
         title: "här börjar dit äventyr.",
         message: "Välkommen till din berättelse! du kommer att göra val som formar din resa."
+    })
+})
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+    const page = findpage(id)
+    
+
+    res.render("story.njk", {
+        page
     })
 })
 
